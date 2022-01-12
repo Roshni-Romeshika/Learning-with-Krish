40fb7ea2a694a4e87d64396b3ae411692f137e0f
Your Name <you@example.com>
2022-01-12 16:07:48 +0530

    added project files - config-server

---
+++ Microservices/git-backend-config-server-consummer/config-server/src/test/java/com/roshni/rentcloud/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roshni.rentcloud.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
